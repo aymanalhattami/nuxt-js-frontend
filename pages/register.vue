3c222f2bc2c6410f5eee6bd0d51457c5c8404c1e
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-01-20 11:27:49 +0300

    simplify form validation with FormKit

--- pages/register.vue
+++ pages/register.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import {RegisterPayload} from "~/types";
+import {FormKitNode} from "@formkit/core";
+import {AxiosError} from "axios";
+
 definePageMeta({
   layout: "centered",
 });
@@ -12,36 +16,50 @@ const form = ref({
 
 const { register } = useAuth();
 
+async function handleRegister(payload: RegisterPayload, node: FormKitNode){
+    try{
+        await register(payload);
+    }catch (e) {
+        if(e instanceof AxiosError && e.response?.status === 422){
+            node.setErrors([], e.response.data.errors);
+        }
+    }
+}
+
 </script>
 <template>
   <div class="register">
     <h1>Register</h1>
-      <pre>
-          {{ form }}
-      </pre>
-    <form @submit.prevent="register(form)">
-      <label>
-        <div>Name</div>
-        <input v-model="form.name" type="text" />
-      </label>
+<!--    <form @submit.prevent="register(form)">-->
+<!--      <label>-->
+<!--        <div>Name</div>-->
+<!--        <input v-model="form.name" type="text" />-->
+<!--      </label>-->
 
-      <label>
-        <div>Email</div>
-        <input v-model="form.email" type="email" />
-      </label>
+<!--      <label>-->
+<!--        <div>Email</div>-->
+<!--        <input v-model="form.email" type="email" />-->
+<!--      </label>-->
 
-      <label>
-        <div>Password</div>
-        <input v-model="form.password" type="password" />
-      </label>
+<!--      <label>-->
+<!--        <div>Password</div>-->
+<!--        <input v-model="form.password" type="password" />-->
+<!--      </label>-->
 
-      <label>
-        <div>Confirm Password</div>
-        <input v-model="form.password_confirmation" type="password" />
-      </label>
+<!--      <label>-->
+<!--        <div>Confirm Password</div>-->
+<!--        <input v-model="form.password_confirmation" type="password" />-->
+<!--      </label>-->
 
-      <button class="btn">Register</button>
-    </form>
+<!--      <button class="btn">Register</button>-->
+<!--    </form>-->
+
+      <FormKit type="form" submit-label="Register" @submit="handleRegister">
+          <FormKit type="text" label="Name" name="name" />
+          <FormKit type="email" label="Email" name="email" />
+          <FormKit type="password" label="Password" name="password" />
+          <FormKit type="password" label="Password" name="password_confirmation" />
+      </FormKit>
 
     <p>
       Already have an account?
